7626bbda31ac577b9cb5bde0493ce38e61f80dc6
mecha-aima <aimo.malik77@gmail.com>
2025-05-03 14:53:44 +0500

    tailwindcss setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,14 @@ function App() {
       .catch(err => console.error('Fetch error:', err));
   }, []);
 
-  return <div>CORS is working!</div>;
+  return (
+    <div className="bg-teal-20 text-navy-80 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold underline mb-4">Hello world!</h1>
+      <p className=" text-xl">
+        This is a custom themed component.
+      </p>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx,css}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1a73e8',
+          secondary: '#fbbc05',
+          accent: '#34a853',
+          background: '#f1f3f4',
+          // etc.
+        },
+      },
+    },
+    plugins: [],
+  }

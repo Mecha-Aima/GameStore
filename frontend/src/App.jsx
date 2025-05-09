25c78fad022d29daaa558a2aa0feaae22993b3ca
Syed-Nalain <syednalain3021@gmail.com>
2025-05-10 02:55:56 +0500

    added routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,28 @@
 import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Auth from './pages/Auth';
+import Home from './pages/Home';
+import OrderSummary from './pages/OrderSummary';
 
 function App() {
   useEffect(() => {
     fetch('/api/test')
       .then(res => res.json())
       .then(data => {
-        console.log(data); // Should log: { message: "CORS is working!" }
+        console.log(data); 
       })
       .catch(err => console.error('Fetch error:', err));
   }, []);
 
-  return <div>CORS is working!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/ordersummary" element={<OrderSummary />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
